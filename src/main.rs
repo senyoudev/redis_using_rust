b832888aaba8d615ef623a88c503e4b9cfb91fd1
senyoudev <younesmeskafe2018@gmail.com>
2024-03-02 15:24:46 +0100

    expiry

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::{Read, Write};
-use std::string;
+use std::time;
 use std::thread::spawn;
 use std::collections::HashMap;
+use std::time::Duration;
 
 
 
@@ -11,7 +12,7 @@ use std::collections::HashMap;
 
 fn main() {
     // we define a key-value data structure to store and retrieve the items (SET-GET) => we use a hashmap
-    let mut data_store : HashMap<String, String> = HashMap::new();    
+    let mut data_store : HashMap<String, (String, Option<time::SystemTime>)> = HashMap::new();    
  
     // Create a TCP listener and bind it to the address
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
@@ -36,7 +37,7 @@ fn main() {
 }
 
 
-fn handle_client(mut _stream: TcpStream, mut data_store: HashMap<String, String>) {
+fn handle_client(mut _stream: TcpStream, mut data_store: HashMap<String, (String, Option<time::SystemTime>)>) {
 
     // now we implement a proper redis protocol
 
@@ -76,9 +77,17 @@ fn handle_client(mut _stream: TcpStream, mut data_store: HashMap<String, String>
                     }
                     "set" => {
                         // the command will be like : *3\r\n$3\r\nset\r\n$3\r\nkey\r\n$5\r\nvalue\r\n so the key will be in position 4 and the value will be in position 6
+                        // normally we have the argument px about expiry so if the user specifies the expiry, the command will be as *3\r\n$3\r\nset\r\n$3\r\nkey\r\n$5\r\nvalue\r\n$2\r\npx\r\n$3\r\n100\r\n
                         let key = command_raw_vec[4];
                         let value = command_raw_vec[6];
-                        data_store.insert(key.to_string(), value.to_string());
+                        let expiry_index = command_raw_vec.iter().position(|&x| x == "px");
+                        let mut expired_time : Option<time::SystemTime> = None;
+                        if let Some(index) = expiry_index {
+                            if let Ok(expiry) = command_raw_vec[index + 1].parse::<u64>() {
+                                expired_time = Some(time::SystemTime::now() + Duration::from_millis(expiry));
+                            }
+                        }
+                        data_store.insert(key.to_string(), (value.to_string(),expired_time));
                         let res = format!("{}{}", "+OK", separator); // res is +OK\r\n
                         println!("set command response: {:?}", res);
                         _stream
@@ -88,8 +97,20 @@ fn handle_client(mut _stream: TcpStream, mut data_store: HashMap<String, String>
                     "get" => {
                         // the command will be like : *2\r\n$3\r\nget\r\n$3\r\nkey\r\n so the key will be in position 4
                         let key = command_raw_vec[4];
-                        //check if the key exists in the data store
-                        if let Some(value) = data_store.get(key) {
+                        //check if the key exists in the data store and it is not expired
+
+
+                        if let Some((value,expired_time)) = data_store.get(key) {
+                            let now = time::SystemTime::now();
+                            if expired_time.is_some() && now > expired_time.unwrap() {
+                                data_store.remove(key);
+                                let res = format!("{}{}", "$-1", separator); // res is $-1\r\n
+                                println!("get command response: {:?}", res);
+                                _stream
+                                    .write_all(res.as_bytes())
+                                    .expect("Failed to write response");
+                                continue;
+                            }
                             let res = format!("${}{}{}{}", value.len(), separator, value, separator); // res is $5\r\nvalue\r\n
                             println!("get command response: {:?}", res);
                             _stream
